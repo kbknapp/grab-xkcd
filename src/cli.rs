7a29cf13e2c736c556ecbbd57d98486e60f24d77
Kevin K <kbknapp@gmail.com>
2021-01-08 12:22:36 -0500

    implements runtime shell completions

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,5 @@
-use clap::Clap;
+use clap::{Clap, IntoApp};
+use clap_generate::{generate, generators::*};
 
 /// A utility to grab XKCD comics
 #[derive(Clap)]
@@ -15,6 +16,32 @@ pub struct Args {
     /// Save image file to current directory
     #[clap(long, short)]
     pub save: bool,
+    /// Generate a SHELL completion script and print to stdout
+    #[clap(long, short, arg_enum, value_name = "SHELL")]
+    pub completions: Option<Shell>,
+}
+
+#[derive(Clap, Copy, Clone)]
+pub enum Shell {
+    Bash,
+    Zsh,
+    Fish,
+    PowerShell,
+    Elvish,
+}
+
+impl Shell {
+    pub fn generate(&self) {
+        let mut app = Args::into_app();
+        let mut fd = std::io::stdout();
+        match self {
+            Shell::Bash => generate::<Bash, _>(&mut app, "grab-xkcd", &mut fd),
+            Shell::Zsh => generate::<Zsh, _>(&mut app, "grab-xkcd", &mut fd),
+            Shell::Fish => generate::<Fish, _>(&mut app, "grab-xkcd", &mut fd),
+            Shell::PowerShell => generate::<PowerShell, _>(&mut app, "grab-xkcd", &mut fd),
+            Shell::Elvish => generate::<Elvish, _>(&mut app, "grab-xkcd", &mut fd),
+        }
+    }
 }
 
 #[derive(Clap, Copy, Clone)]
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,12 @@ const BASE_URL: &str = "https://xkcd.com";
 
 fn main() -> Result<()> {
     let args = cli::Args::parse();
+
+    if let Some(shell) = args.completions {
+        shell.generate();
+        std::process::exit(0);
+    }
+
     let client = client::XkcdClient::new(args);
     client.run()
 }
